2e49fca431cfafdda57e175aff3e6ed5d06249d5
Robert Scholte <rfscholte@apache.org>
2012-09-09 11:13:42 +0000

    Add verification scripts for MSITESKIN-9 ITs
    git-svn-id: file:///mojohaus-svn-repo/trunk/mojo/siteskinner-maven-plugin@17476 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/it/projects/msiteskin-9_other-parent/verify.groovy
@@ -0,0 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+def siteXml =new File( basedir, 'target/siteskinner/src/site/site.xml') 
+assert siteXml.exists() 
+def projectNode = new XmlSlurper().parse( siteXml )
+assert projectNode.skin.size() == 1
+assert projectNode.skin.groupId == 'org.apache.maven.skins'
+assert projectNode.skin.artifactId == 'maven-fluido-skin'
+assert projectNode.skin.version == '1.0'
